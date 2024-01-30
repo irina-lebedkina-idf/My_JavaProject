0fae3c9515f3c0ac28b112af63d67c7233ccfc7f
“irina-lebedkina“ <irina.lebedkina@id.finance.com>
2024-01-30 10:25:08 +0300

    Initial commit

---
+++ src/First.java
@@ -0,0 +1,19 @@
+
+//ДЗ_1
+public class First {
+    public static void main (String[] args) {
+        System.out.println("                  РУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя вправда открытая мной.");
+        System.out.println("                                 О.Кайям.");
+
+//От Сергея (переменные)
+        String x = "IRA";
+        String y = "vova";
+        String love = "vb ngfngfnmgfnhg";
+//        System.out.println(x + " " + " + " + x + " = " + love );
+
+    }
+}
